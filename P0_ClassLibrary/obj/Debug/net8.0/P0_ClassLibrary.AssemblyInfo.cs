c913a903cdbf16db38ab3547cbd1a1e5b6ca1fab
Irfan Yousuf <irfanusuf33@gmail.com>
2025-11-11 12:22:39 +0530

    fixes

--- P0_ClassLibrary/obj/Debug/net8.0/P0_ClassLibrary.AssemblyInfo.cs
+++ P0_ClassLibrary/obj/Debug/net8.0/P0_ClassLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P0_ClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3da886f57f50aa434c1896bb2f3f17a758b227d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63083e6b41d27f51b9f09c2cb658e45f2871fb74")]
 [assembly: System.Reflection.AssemblyProductAttribute("P0_ClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P0_ClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
